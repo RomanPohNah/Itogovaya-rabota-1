1edd44c8dbfd3d3e6331f9386a6cf055e24e88b3
Roman <romanpokolenko@yandex.ru>
2024-01-27 13:16:31 +0900

    Создаём массив строк длина которых <=3

--- itogovaya1/Program.cs
+++ itogovaya1/Program.cs
@@ -1,1 +1,11 @@
 ﻿string[] originalArray = { "fff", "a3dfr", "$", "jytr", "343"};
+string[] newArray = new string[originalArray.Length];
+int j = 0;
+for (int i = 0; i < originalArray.Length; i++)
+{
+    if (originalArray[i].Length <= 3)
+    {
+        newArray[j] = originalArray[i];
+        j++;
+    }
+}
